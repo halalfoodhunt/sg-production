ae07902987c3b1379b833f0b66434bbb8cea30e5
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-23 03:14:44 +0000

    edited

--- app/admin/merchants.rb
+++ app/admin/merchants.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Merchant do
   permit_params [:registered_name_of_business, :name, :position, :email, :password, :business_registration_number, :office_number, :mobile_number, :office_mailing_address]
   
-  before_filter :only => [:show] do
+  before_filter :only => [:show, :edit] do
     @merchant = Merchant.friendly.find(params[:id])
   end
 
