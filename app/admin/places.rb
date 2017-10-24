0b070786be0e98eea12cbc0f2fab1d226b31c841
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-24 10:19:00 +0000

    Added Qualifying Type

--- app/admin/places.rb
+++ app/admin/places.rb
@@ -82,6 +82,8 @@ ActiveAdmin.register Place do
     actions
   end
   
+  column :place
+  column(:reward) { |place| place.reward.terms }
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
