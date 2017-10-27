ff892d071cb073171a3ccf58b4eb71033b44f3d2
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-27 12:41:42 +0000

    added raw foods

--- app/admin/raw_foods.rb
+++ app/admin/raw_foods.rb
@@ -15,7 +15,7 @@ ActiveAdmin.register RawFood do
     end
     
     form do |f|
-        f.inputs "raw_food" do
+        f.inputs "Raw Food" do
         f.input :logo, :as => :file, :hint => image_tag(f.object.logo.url(:medium)) 
         f.input :featured_image, :as => :file, :hint => image_tag(f.object.featured_image.url(:thumb)) 
         f.input  "merchant"
