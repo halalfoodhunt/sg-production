e136f17e8a0adcf32653d85176076b4ff75ae329
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-30 16:12:59 +0000

    added Day

--- app/admin/features.rb
+++ app/admin/features.rb
@@ -7,6 +7,8 @@ ActiveAdmin.register Feature do
          end
       f.actions
     end
+    
+    
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
