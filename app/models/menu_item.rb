a3fc77852a32118276b3199e69e3060a84993347
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-30 17:38:59 +0000

    added Day

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -2,5 +2,5 @@ class MenuItem < ActiveRecord::Base
     has_many :dish_items
 	has_many :places, :through => :dish_items
 	has_many :bakers, :through => :dish_items
-	has_many :online_grocers, :through => :dish_items
+	has_many :online_retails, :through => :dish_items
 end
