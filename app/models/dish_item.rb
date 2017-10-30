99790f9e4e41ddfc1d95226580f45bd97a3dcb44
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-30 17:36:44 +0000

    added Day

--- app/models/dish_item.rb
+++ app/models/dish_item.rb
@@ -1,11 +1,6 @@
 class DishItem < ActiveRecord::Base
     belongs_to :menu_item
 	belongs_to :place
-	belongs_to :homy
-	belongs_to :food_delivery
 	belongs_to :online_retail
 	belongs_to :baker
-	belongs_to :supplier
-	belongs_to :raw_food
-	belongs_to :caterer
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,7 +1,6 @@
 class MenuItem < ActiveRecord::Base
     has_many :dish_items
 	has_many :places, :through => :dish_items
-	has_many :homies, :through => :dish_items
-	has_many :food_deliveries, :through => :dish_items
+	has_many :bakers, :through => :dish_items
 	has_many :online_grocers, :through => :dish_items
 end
