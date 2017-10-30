614200245cec1c49fab89b6838b821bfb42c7a1e
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-30 17:29:28 +0000

    added Day

--- app/admin/cuisine_type.rb
+++ app/admin/cuisine_type.rb
@@ -1,19 +1,4 @@
-ActiveAdmin.register MenuItem do
-    permit_params [:name]
-    
-    form do |f|
-      f.inputs :menu_item do
-        f.input  "name"
-         end
-      f.actions
-    end
-    
-    index do
-    selectable_column
-    column  "id"
-    column  "name"
-    actions
-    end
+ActiveAdmin.register CuisineType do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
