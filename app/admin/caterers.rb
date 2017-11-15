2039c0ab5a82f44bd2d097241c6df40d0d5eecd9
Allyssa Deanna <dee@halalfoodhunt.com>
2017-11-15 11:22:44 +0000

    edited raw foods

--- app/admin/caterers.rb
+++ app/admin/caterers.rb
@@ -41,7 +41,7 @@ ActiveAdmin.register Caterer do
         f.inputs do
         f.has_many :rewards,
             allow_destroy: true,
-            new_record: false do |a|
+            new_record: true do |a|
               a.input :discount
               a.input :terms
               a.actions
