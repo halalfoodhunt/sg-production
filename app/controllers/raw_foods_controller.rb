cb6c67329a6e935c8fc394f54480cc3a880c424e
Allyssa Deanna <dee@halalfoodhunt.com>
2017-11-15 02:43:59 +0000

    edited raw foods

--- app/controllers/raw_foods_controller.rb
+++ app/controllers/raw_foods_controller.rb
@@ -83,7 +83,7 @@ class RawFoodsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_raw_food
-      @raw_food = RawFood.find(params[:id])
+      @raw_food = RawFood.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
