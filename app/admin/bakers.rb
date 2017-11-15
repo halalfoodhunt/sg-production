3abe84b8a6fb4cdaa16af8a1bee59a36e5d80fb9
Allyssa Deanna <dee@halalfoodhunt.com>
2017-11-15 08:57:59 +0000

    edited raw foods

--- app/admin/bakers.rb
+++ app/admin/bakers.rb
@@ -54,7 +54,7 @@ ActiveAdmin.register Baker do
         f.inputs do
         f.has_many :rewards,
             allow_destroy: true,
-            new_record: false do |a|
+            new_record: true do |a|
               a.input :discount
               a.input :terms
               a.input :friends_code
