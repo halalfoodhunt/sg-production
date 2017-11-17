2fab014fb8a4e8d27f1f53b8e78eab7449617dcb
Allyssa Deanna <dee@halalfoodhunt.com>
2017-11-17 06:15:04 +0000

    edited

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,24 @@ class PlacesController < ApplicationController
   # GET /places
   # GET /places.json
   def index
-    @places = Place.all
+    @search_places = Place.ransack(params[:q])
+    @places = @search_places.result.order("created_at DESC").where(draft: false)
+    @search_homies = Homy.ransack(params[:q])
+    @homies = @search_homies.result.order("created_at DESC").where(draft: false)
+    @search_caterers = Caterer.ransack(params[:q])
+    @caterers = @search_caterers.result.order("created_at DESC").where(draft: false)
+    @search_food_deliveries = FoodDelivery.ransack(params[:q])
+    @food_deliveries = @search_food_deliveries.result.order("created_at DESC").where(draft: false)
+    @search_online_retails = OnlineRetail.ransack(params[:q])
+    @online_retails = @search_online_retails.result.order("created_at DESC").where(draft: false)
+    @search_suppliers = Supplier.ransack(params[:q])
+    @suppliers = @search_suppliers.result.order("created_at DESC").where(draft: false)
+    @search_raw_foods = RawFood.ransack(params[:q])
+    @raw_foods = @search_raw_foods.result.order("created_at DESC").where(draft: false)
+    @search_bakers = Baker.ransack(params[:q])
+    @bakers = @search_bakers.result.order("created_at DESC").where(draft: false)
+    @place_types = PlaceType.all
+    @discounts = Discount.all
   end
 
   # GET /places/1
