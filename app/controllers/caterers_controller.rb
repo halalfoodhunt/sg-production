8eb6ca2c71b7b65c8daffdfc3a51831dff3b1bb8
Allyssa Deanna <dee@halalfoodhunt.com>
2017-12-15 13:21:08 +0000

    edited

--- app/controllers/caterers_controller.rb
+++ app/controllers/caterers_controller.rb
@@ -5,12 +5,30 @@ class CaterersController < ApplicationController
   # GET /caterers
   # GET /caterers.json
   def index
+    if params[:caterer_type].present? 
+    @caterer_type_id = CatererType.find_by(name: params[:caterer_type]).id
+    @caterers = Caterer.joins(:caterer_categories).where(caterer_categories: {caterer_type_id: @caterer_type_id})
+    @search_caterers = Caterer.ransack(params[:q])
+    elsif params[:menu_item].present? 
+    @caterer_service_type_id = CatererServiceType.find_by(name: params[:caterer_service_type]).id
+    @caterers = Caterer.joins(:cooking_service_types).where(cooking_service_types: {caterer_service_type_id: @caterer_service_type_id})
+    @search_caterers = Caterer.ransack(params[:q])
+    elsif params[:cuisine_type].present? 
+    @cuisine_type_id = CookingType.find_by(name: params[:cuisine_type]).id
+    @caterers = Caterer.joins(:cooking_types).where(cooking_types: {cuisine_type_id: @cuisine_type_id})
+    @search_caterers = Caterer.ransack(params[:q])
+    elsif params[:event_type].present? 
+    @event_type_id = DiningType.find_by(name: params[:event_type]).id
+    @caterers = Caterer.joins(:function_types).where(function_types: {event_type_id: @event_type_id})
+    @search_caterers = Caterer.ransack(params[:q])
+    else
+    @search_caterers = Caterer.ransack(params[:q])
+    @caterers = @search_caterers.result.order("created_at DESC").where(draft: false)
+    end
     @search_places = Place.ransack(params[:q])
     @places = @search_places.result.order("created_at DESC").where(draft: false)
     @search_homies = Homy.ransack(params[:q])
     @homies = @search_homies.result.order("created_at DESC").where(draft: false)
-    @search_caterers = Caterer.ransack(params[:q])
-    @caterers = @search_caterers.result.order("created_at DESC").where(draft: false)
     @search_food_deliveries = FoodDelivery.ransack(params[:q])
     @food_deliveries = @search_food_deliveries.result.order("created_at DESC").where(draft: false)
     @search_online_retails = OnlineRetail.ransack(params[:q])
